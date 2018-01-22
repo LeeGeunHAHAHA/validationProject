08a2114557a254ce7acc64ae6916a42e169e8492
lee <lee@localhost.localdomain>
2018-01-22 16:08:26 +0900

    make skeleton for IOTest

---
+++ app/tests/IOTest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from random import *
+class IOTest(Testable):
+	def getOption():
+		return 0
+	def readyReset():
+		return 0
+	def runTest():
+		return 0
+	def vfEnable():
+		return 0
+	def MakeQueue():
+		return 0
+	def seqTest():
+		return 0
+	def randomTest():
+		return 0
+	def stopTest():
+		return 0
+
+
+
+if __name__ == "__main__":
+	main()
---
+++ app/tests/Interfaces.py
@@ -0,0 +1,7 @@
+class Testable(object):
+    def runTest(self):
+        pass
+
+class Resetable(Testable):
+    def readReset(self):
+        pass
