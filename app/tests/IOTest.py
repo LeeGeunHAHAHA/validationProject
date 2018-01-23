333ce7e42ea89d414e060b238d0da5704a30b793
DESKTOP-SLICT8I\dnjsw <dnjswl01@hanmail.,net>
2018-01-23 11:00:25 +0900

    pydocs test

--- app/tests/IOTest.py
+++ app/tests/IOTest.py
@@ -7,6 +7,12 @@ class IOTest(Testable):
 		'''
             To initailize member variable, this function requests information to user
         '''
+		port =0  # tmp
+		target = 1 # tmp
+		target2 = 2  # tmp
+		target1_file = open("./iport" + port + "/target" + target, 'w')
+		target2_file = open("./iport" + port + "/target" + target2, 'w')
+		nvme_file = open("./proc/vlun/nvme", 'w')
 		return 0
 
 
@@ -24,17 +30,47 @@ class IOTest(Testable):
 		return 0
 
 
-	def vfEnable():
+	def vfEnable(target2, numvf,target1_file, target2_file):
 		'''
             This function sets number of virtual function
+            :param:
+            :return:
 	    '''
+
+		target1_file.write("NumVFs=" + numvf)
+
+		if (target2 != ""):
+			target2_file.write("NumVFs=" + numvf)
 		return 0
 
 
-	def MakeQueue():
+	def MakeQueue(target, target2, starttarget, endtarget2, endtarget, port, queuecnt, queuedepth, nvme_file):
 		'''
 			This function sets Queue Depth and num of Queue
 		'''
+		queuetarget = 0
+
+		for looptarget in range(starttarget, endtarget2):
+			# for VF
+			if (looptarget <= endtarget):
+				targetN_file = open("./iport" + port + "/target" + looptarget, 'w')
+				targetN_file.write("QueueCount=" + queuecnt)
+				targetN_file.write("QueueDepth=" + queuedepth)
+				targetN_file.write("QueueAlignment=0")
+				nvme_file.write("restart=" + looptarget)
+
+			# for PF
+			else:
+				if (looptarget == endtarget + 1):
+					queuetarget = target
+				else:
+					queuetarget = target2
+
+				queuetarget_file = open("./iport" + port + "/target" + queuetarget, 'w')
+				queuetarget_file.write("QueueCount=" + queuecnt)
+				queuetarget_file.write("QueueDepth=" + queuedepth)
+				queuetarget_file.write("QueueAlignment=0")
+				nvme_file.write("restart=" + queuetarget)
 		return 0
 
 
--- app/tests/Reset.py
+++ app/tests/Reset.py
@@ -2,81 +2,77 @@
 This module has Classes of Reset. Reset has child classes.
 """
 
-class Reset(Testable)
-	"""
+class Reset(Testable):
+    '''
 	This class is parent class of child classes.
-	"""
+	'''
 
 
-
-	def runTest():
-	    '''
+    def runTest():
+        '''
 	    This function calls method to reset VF or PF
 	    :return:
 	    '''
-	    return 0
+        return 0
 
-	def startTest():
-	    '''
-	    This function starts a I/O test
-	    :return:
-	    '''
-	    return 0
+    def startTest():
+        '''
+        This function starts a I/O test
+        :return:
+        '''
+        return 0
 
-	def pollForTestStatus():
-	    '''
-	    This function checks the status of test
-	    :return:
-	    '''
-	    return 0
+    def pollForTestStatus():
+        '''
+        This function checks the status of test
+        :return:
+        '''
+        return 0
 
-	def getResults():
-	    ''''
-	    This function gets results
-	    :return:
-	    '''
-	    return 0
+    def getResults():
+        ''''
+        This function gets results
+        :return:
+        '''
+        return 0
 
-	def doAction():
-	    '''
-	    This function starts the reset test
-	    :return:
-	    '''
-	    return 0
+    def doAction():
+        '''
+        This function starts the reset test
+        :return:
+        '''
+        return 0
 
-	def getTimings():
-	    '''
-	    This function gets a actual time
-	    :return:
-	    '''
-	    return 0
+    def getTimings():
+        '''
+        This function gets a actual time
+        :return:
+        '''
+        return 0
 
-	def quarchGlitch():
-	    ''''
-	    This function starts glitch
-	    :return:
-	    '''
-	    return 0
+    def quarchGlitch():
+        ''''
+        This function starts glitch
+        :return:
+        '''
+        return 0
 
-	def stopTest():
-	    '''
-	    This function stops all reset tests
-	    :return:
-	    '''
-	    return 0
+    def stopTest():
+        '''
+        This function stops all reset tests
+        :return:
+        '''
+        return 0
 
 class SubsystemReset(Reset):
 
+    def runTest():
+        return 0
 
 
 
-	def runTest():
-		return 0
-
-
-
-	def doAction():
-		return 0
+    def doAction():
+        return 0
 	
 class FLR(Reset):
 
