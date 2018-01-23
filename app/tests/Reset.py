c94ea54fc2655d60be886bc7848c25ed4a2c932f
LeeGeunHAHAHA <ygh1kr@naver.com>
2018-01-23 10:55:46 +0900

    reset change to class

--- app/tests/Reset.py
+++ app/tests/Reset.py
@@ -1,55 +1,118 @@
-def runTest():
-    '''
-    This function calls method to reset VF or PF
-    :return:
-    '''
-    return 0
+"""
+This module has Classes of Reset. Reset has child classes.
+"""
 
-def startTest():
-    '''
-    This function starts a I/O test
-    :return:
-    '''
-    return 0
+class Reset(Testable)
+	"""
+	This class is parent class of child classes.
+	"""
 
-def pollForTestStatus():
-    '''
-    This function checks the status of test
-    :return:
-    '''
-    return 0
 
-def getResults():
-    ''''
-    This function gets results
-    :return:
-    '''
-    return 0
 
-def doAction():
-    '''
-    This function starts the reset test
-    :return:
-    '''
-    return 0
+	def runTest():
+	    '''
+	    This function calls method to reset VF or PF
+	    :return:
+	    '''
+	    return 0
 
-def getTimings():
-    '''
-    This function gets a actual time
-    :return:
-    '''
-    return 0
+	def startTest():
+	    '''
+	    This function starts a I/O test
+	    :return:
+	    '''
+	    return 0
 
-def quarchGlitch():
-    ''''
-    This function starts glitch
-    :return:
-    '''
-    return 0
+	def pollForTestStatus():
+	    '''
+	    This function checks the status of test
+	    :return:
+	    '''
+	    return 0
 
-def stopTest():
-    '''
-    This function stops all reset tests
-    :return:
-    '''
-    return 0
+	def getResults():
+	    ''''
+	    This function gets results
+	    :return:
+	    '''
+	    return 0
+
+	def doAction():
+	    '''
+	    This function starts the reset test
+	    :return:
+	    '''
+	    return 0
+
+	def getTimings():
+	    '''
+	    This function gets a actual time
+	    :return:
+	    '''
+	    return 0
+
+	def quarchGlitch():
+	    ''''
+	    This function starts glitch
+	    :return:
+	    '''
+	    return 0
+
+	def stopTest():
+	    '''
+	    This function stops all reset tests
+	    :return:
+	    '''
+	    return 0
+
+class SubsystemReset(Reset):
+
+
+
+
+	def runTest():
+		return 0
+
+
+
+	def doAction():
+		return 0
+	
+class FLR(Reset):
+
+
+
+
+	def runTest():
+		return 0
+
+
+
+	def doAction():
+		return 0
+	
+class ControllerReset(Reset):
+
+
+
+
+	def runTest():
+		return 0
+
+
+
+	def doAction():
+		return 0
+	
+class PERST(Reset):
+
+
+
+
+	def runTest():
+		return 0
+
+
+
+	def doAction():
+		return 0
