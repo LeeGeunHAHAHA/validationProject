ee7615d18ee5892acab6f044c7138aa0a746d494
DESKTOP-SLICT8I\dnjsw <dnjswl01@hanmail.,net>
2018-02-01 10:02:08 +0900

    pushhhh

--- app/tests/IOEach.py
+++ app/tests/IOEach.py
@@ -46,53 +46,48 @@ def makeIOTestQueue(functionList):
 
 
 
-#class IOTest():
-#    """
-#    This class have information about I/O sequential/ I/O random test.
-#    When initiate this class, user can run test RunTest method.
-#    """
-#    port = None
-#    targetNum = None
-#    MAXLBA = 65536
-#    limit_size = None
-#    testType = ""   # read, write, compare
-#    port_file = None
-#    startPos =None
-#    idfunc = None
-    #nvme_file = open("./proc/vlun/nvme", 'w')
-
-#    def __init__(self, function, startPos):
-#        self.targetNum = function.targetNum
-#        self.limit_size = 2048
-#        self.port = function.port
-#        self.startPos = startPos
-#        self.idfunc = function
-#        self.testType = input("testType (read | write | compare) : ")
-
-
-#    def RunTest(self):
-#        '''
-#            This function runs a I/O test
-#            :param
-#        '''
-#        self.StartTest()
-
- #   def StartTest(self):
-#        '''
-#            This function starts a sequential test
-#            :param
-#        '''
-#        port_file = open("/iport"+self.port+"/port", 'w')
-#        target = open("/iport0/target" + str(self.targetNum), 'w')
-#        port_file.write("Testlimits=" + str(self.limit_size) + "," + str(self.startPos) + ",0 \n")
-#        target.write("WriteEnabled=1 \n")
-#        target = open("/iport0/target" + str(self.targetNum) + "lun1", 'w')
-#        target.write(self.testType + str(self.targetNum) + ",1,1,0,1,0,0,0,-1,60,0,1,1,0,1:1,1:1,0,-0 ")
-
-#    def StopTest(self):
-#        '''
-#            This function stops all tests
-#            :param
-#        '''
-#        targetN_file = open("/iport0/target"+str(self.targetNum),"w")
-#        targetN_file.write("StopTests ")
+class IOTest():
+    """
+    This class have information about I/O sequential/ I/O random test.
+    When initiate this class, user can run test RunTest method.
+    """
+    port = None
+    targetNum = None
+    MAXLBA = 65536
+    limit_size = None
+    testType = ""   # read, write, compare
+    port_file = None
+    startPos =None
+    idfunc = None
+   #nvme_file = open("./proc/vlun/nvme", 'w')
+    def __init__(self, function, startPos):
+        self.targetNum = function.targetNum
+        self.limit_size = 2048
+        self.port = function.port
+        self.startPos = startPos
+        self.idfunc = function
+        self.testType = input("testType (read | write | compare) : ")
+    def RunTest(self):
+        '''
+            This function runs a I/O test
+            :param
+        '''
+        self.StartTest()
+#   def StartTest(self):
+        '''
+            This function starts a sequential test
+            :param
+        '''
+        port_file = open("/iport"+self.port+"/port", 'w')
+        target = open("/iport0/target" + str(self.targetNum), 'w')
+        port_file.write("Testlimits=" + str(self.limit_size) + "," + str(self.startPos) + ",0 \n")
+        target.write("WriteEnabled=1 \n")
+        target = open("/iport0/target" + str(self.targetNum) + "lun1", 'w')
+        target.write(self.testType + str(self.targetNum) + ",1,1,0,1,0,0,0,-1,60,0,1,1,0,1:1,1:1,0,-0 ")
+    def StopTest(self):
+        '''
+            This function stops all tests
+            :param
+        '''
+        targetN_file = open("/iport0/target"+str(self.targetNum),"w")
+        targetN_file.write("StopTests ")
